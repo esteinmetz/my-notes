e6b2aa1194aae20d1317de6555047fcb89e61b83
Erik Steinmetz <17165462+esteinmetz@users.noreply.github.com>
2025-03-25 10:48:17 -0400

    Quartz sync: Mar 25, 2025, 10:48 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "/res/templates"],
+    ignorePatterns: ["private", "templates", ".obsidian", "/res/template"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
